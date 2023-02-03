779ae67c7397fe4299d823980ff1045d502e3641
jor-eldevera <jorell.devera@gmail.com>
2023-02-02 16:17:47 -0800

    Add click event listeners to each <li> Write clearContent function to clear the children of a div

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import appendHomeToContent from "./appendHomeToContent";
 import "./styles.css";
 
-console.log("This is a test.");
-
 // This is the div#content tag
 const content = document.getElementById("content");
+appendHomeToContent(content); // Initial homepage appending
 
-appendHomeToContent(content);
+// Clears the div of all content
+function clearContent(div) {
+    while (div.firstChild) {
+        div.removeChild(div.lastChild);
+    }
+}
+
+const homeLi = document.getElementById("home");
+homeLi.addEventListener("click", function() {
+    clearContent(content);
+    appendHomeToContent(content);
+});
+
+const menuLi = document.getElementById("menu");
+menuLi.addEventListener("click", function() {
+
+});
+
+const contactLi = document.getElementById("contact");
+contactLi.addEventListener("click", function() {
+
+});
+
